562a2cc90c224f60c314ce2b756b5060685f4944
pbscrimmage <patrick@brooklynresearch.org>
2017-03-11 00:00:17 -0500

    sorting record list by date

--- src/providers/database.ts
+++ src/providers/database.ts
@@ -30,7 +30,7 @@ export class Database {
 
     public getRecords() {
         return new Promise((resolve, reject) => {
-            this.storage.executeSql("SELECT * FROM records", []).then((data) => {
+            this.storage.executeSql("SELECT * FROM records ORDER BY date ASC", []).then((data) => {
                 let records = [];
                 if(data.rows.length > 0) {
                     for(let i = 0; i < data.rows.length; i++) {
